81c0749230d09c7e92b9b9ead97b75ff13bcb530
illonage <zanolli.geraldine@gmail.com>
2016-09-05 12:32:08 +0100

    test caption

--- instagram.js
+++ instagram.js
@@ -181,7 +181,7 @@
                       var dateFinal =  (date.getMonth()+1) +"/"+date.getDate()+"/"+ date.getFullYear()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
             //var d = new Date (dateFinal);
           }
-              if (feat[i].caption.text && feat[i].caption.text !== "null" && feat[i].caption.text !== "undefined") {
+              if (data.caption.text && data.caption.text !== "null" && data.caption.text !== "undefined") {
                 var text = feat[i].caption.text;
               }
               else var text = " ";
