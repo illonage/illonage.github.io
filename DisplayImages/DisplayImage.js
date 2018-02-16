a527d94190742fcacdd3f8aa6d2159773848b68e
illonage <zanolli.geraldine@gmail.com>
2018-02-16 16:41:32 +0000

    New Version

--- DisplayImages/DisplayImage.js
+++ DisplayImages/DisplayImage.js
@@ -22,7 +22,9 @@
    function showChooseSheetDialog(){
     $('#choose_sheet_buttons').empty();
     const dashboardName = tableau.extensions.dashboardContent.dashboard.name;
-    $('#choose_sheet_title').text(dashboardName);
+    
+    const textFormat2 = $('<h4><font color="white">Select your sheet with the URL of the image</font></h4>');
+    $('#choose_sheet_buttons').append(textFormat2);
     const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
     worksheets.forEach(function (worksheet) {
       // Declare our new button which contains the sheet name
@@ -35,7 +37,8 @@
 
         // Close the dialog and show the data table for this worksheet
         $('#choose_sheet_dialog').modal('toggle');
-        returnURL(worksheetName);
+        showChooseSelectionDialog(worksheetName);
+        
       });
 
       // Add our button to the list of worksheets to choose from
@@ -47,39 +50,37 @@
 
    }
 
-
-
-  function showImage () {
-    // Clear out the existing list of sheets
-    $('#choose_sheet_buttons').empty();
-
-    // Set the dashboard's name in the title
-    const dashboardName = tableau.extensions.dashboardContent.dashboard.name;
+   function showChooseSelectionDialog(worksheetName){
+    const worksheet = getSelectedSheet(worksheetName);
+    const text = "Select the URL of the image to display";
+    const textFormat = $('<h4><font color="white">Select the field that indicated the URL of the image to display</font></h4>');
     
-
-
-    // The first step in choosing a sheet will be asking Tableau what sheets are available
-    const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
-    
-    // Next, we loop through all of these worksheets add add radio buttons for each one
-    worksheets.forEach(function (worksheet) {
-      // Declare our new button which contains the sheet name
-      if (worksheet.name == "Collection Sales") {
-
-        returnURL(worksheet.name);
-
-      }
+    $('#choose_image_buttons').append(textFormat);
+    //$('#choose_image_dialog').text(worksheet.name);
+    worksheet.getSummaryDataAsync().then(function(data) {
+      const columnsTable = data.columns;
       
+      columnsTable.forEach(function (name) {
+        const button2 = createButton(name.fieldName);
+        button2.click(function () {
+          const fieldName = name.fieldName;
+          $('#choose_image_dialog').modal('toggle');
+          returnURL(worksheetName,fieldName);
+        
+        });
+
+        $('#choose_image_buttons').append(button2);
+      });
     });
-
-     
-
-    // Show the dialog
     
-  }
+    $('#choose_image_dialog').modal('toggle');
+    //returnURL(worksheetName);
+   }
 
 
-  function returnURL(worksheetName){
+  
+
+  function returnURL(worksheetName, fieldName){
     if (unregisterEventHandlerFunction) {
       unregisterEventHandlerFunction();
     }
@@ -89,7 +90,13 @@
     worksheet.getSelectedMarksAsync().then(function (marks) {
       // Get the first DataTable for our selected marks (usually there is just one)
       const worksheetData = marks.data[0];
-      
+      var index = 0;
+      for(var i =0; i<worksheetData.columns.length;i++){
+
+          if (worksheetData.columns[i].fieldName==fieldName) {
+              index = worksheetData.columns[i].index;
+          }
+      }
 
       // Map our data into the format which the data table component expects it
       const data = worksheetData.data.map(function (row) {
@@ -97,35 +104,25 @@
           return cell.formattedValue;
         });
 
-         for(var i =0; i<rowData.length;i++){
-            if (rowData[i].includes("http")){
-              return (rowData[i]);
-            }
-          }
+        return (rowData[index]);
+            
 
         ;
       });
 
-      const name = worksheetData.data.map(function (row) {
-         const rowData = row.map(function (cell) {
-          return cell.formattedValue;
-        });
-        
-          return (rowData[0]);
-
-      });
+      
 
       
 
       // Populate the data table with the rows and columns we just pulled out
-      displayURL(data,name);
+      displayURL(data);
 
 
     });
     
     unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, function (selectionEvent) {
       
-      returnURL(worksheetName);
+      returnURL(worksheetName,fieldName);
     });
 
   }
@@ -138,16 +135,15 @@
   }
 
 
-  function displayURL(URL,name){
+  function displayURL(URL){
     $('#selected_marks').empty();
     var str = URL+" ";
     var names = name +" ";
     var tableNames = names.split(",");
     var tableImages = str.split(",");
-    console.log(tableNames);
     for (var i = 0; i < tableImages.length; i++) {
-     const image = $(`<center><img src="
-      ${tableImages[i]}"width="15%" height="15%"></center>`);
+     const image = $(`<img src="
+      ${tableImages[i]}"width="15%" height="15%">`);
 
      $('#selected_marks').append(image);
    }
@@ -161,7 +157,6 @@
     $(`<button type='button' class='btn btn-default btn-block'>
       ${buttonTitle}
     </button>`);
-
     return button;
   }
 
